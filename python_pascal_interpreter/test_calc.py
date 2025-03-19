dff4e0345db837a1340a18fda911f96b17032243
Managedcode-ux <71963227+Managedcode-ux@users.noreply.github.com>
2025-03-19 12:21:46 +0530

    Added tests for multiplication and division

--- python_pascal_interpreter/test_calc.py
+++ python_pascal_interpreter/test_calc.py
@@ -20,3 +20,29 @@ def test_multivalue_addition(input_string, expected_result):
     interpreter = Interpreter(input_string)
     result = interpreter.expr()
     assert result == expected_result
+
+
+@pytest.mark.parametrize("input_string, exp_res")([
+    ("1*3", 3),
+    ("2*12", 24),
+    ("12*2", 24),
+    ("12*12", 144),
+    ("100*100", 10000)
+])
+def test_multivalue_multiplication(input_string, exp_res):
+    interpreter = Interpreter(input_string)
+    result = interpreter.expr()
+    assert result == exp_res
+
+
+@pytest.mark.parametrize("input_string, exp_res")([
+    ("1%3", "invalid exp"),
+    ("2%12", "invalid exp"),
+    ("12%2", 0),
+    ("12%12", 0),
+    ("100%100", 0)
+])
+def test_multivalue_division(input_string, exp_res):
+    interpreter = Interpreter(input_string)
+    result = interpreter.expr()
+    assert result == exp_res
